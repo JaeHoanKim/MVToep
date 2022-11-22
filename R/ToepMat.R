f07111d71a65233986641c42de7dab96af6c7826
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-11-22 09:47:27 +0900

    Change the function MatKernel to MatK

--- R/ToepMat.R
+++ R/ToepMat.R
@@ -1,4 +1,4 @@
-MatKernel = function(x, y ,l, nu){
+MatK = function(x, y ,l, nu){
    ifelse(abs(x - y) > 0, (sqrt(2 * nu) * abs(x - y) / l)^nu /
              (2^(nu - 1) * gamma(nu)) * besselK(x = abs(x - y) * sqrt(2 * nu)/l, nu = nu),
           1.0)

0b5a3509bf629387a6617e45308754996784b043
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-11-22 10:07:59 +0900

    Added explanations for parameter in MatK

--- R/ToepMat.R
+++ R/ToepMat.R
@@ -1,3 +1,14 @@
+#' Matern covariance function
+#'
+#' @param x vector input
+#' @param y vector input (same length with x)
+#' @param l length scale (non-negative)
+#' @param nu smoothness parameter (non-negative)
+#'
+#' @return
+#' @export
+#'
+#' @examples
 MatK = function(x, y ,l, nu){
    ifelse(abs(x - y) > 0, (sqrt(2 * nu) * abs(x - y) / l)^nu /
              (2^(nu - 1) * gamma(nu)) * besselK(x = abs(x - y) * sqrt(2 * nu)/l, nu = nu),

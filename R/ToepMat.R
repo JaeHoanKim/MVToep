856122fbeb17842f4d94abd05a2b0bd2c31e8b6f
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-11-22 10:08:25 +0900

    Added compatibility check in MatK

--- R/ToepMat.R
+++ R/ToepMat.R
@@ -10,6 +10,9 @@
 #'
 #' @examples
 MatK = function(x, y ,l, nu){
+   if (length(x) != length(y)){
+      stop("the length of x and y should be equal")
+   }
    ifelse(abs(x - y) > 0, (sqrt(2 * nu) * abs(x - y) / l)^nu /
              (2^(nu - 1) * gamma(nu)) * besselK(x = abs(x - y) * sqrt(2 * nu)/l, nu = nu),
           1.0)

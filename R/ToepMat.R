4dc02baebf0702084667d6aef695819e36347a61
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-11-22 09:34:28 +0900

    Generate ToepMat.R

---
+++ R/ToepMat.R
@@ -0,0 +1,10 @@
+# no library, no remove, no source here!
+
+
+## Functions for Toeplitz matrices
+
+MatKernel = function(x, y ,l, nu){
+   ifelse(abs(x - y) > 0, (sqrt(2 * nu) * abs(x - y) / l)^nu /
+             (2^(nu - 1) * gamma(nu)) * besselK(x = abs(x - y) * sqrt(2 * nu)/l, nu = nu),
+          1.0)
+}

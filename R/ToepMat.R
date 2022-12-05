1b0394025f55ab4affbaa445e9a54c8a8cdafe2b
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-12-06 05:55:13 +0900

    Removed eigtol

--- R/ToepMat.R
+++ R/ToepMat.R
@@ -22,7 +22,7 @@ MatK = function(x, y ,l, nu){
 
 #' Multivariate normal samples from Toeplitz-structured Covariance matrix
 #'
-#' @param n number of samples extracted from the distribution (should be even integer)
+#' @param n number of samples extracted from the distribution
 #' @param Sigma covarinace matrix of the distribution. It should be a Toeplitz matrix and the number of rows should be even integer
 #' @param mu the mean vector of the distribtuion
 #' @param eigtol numerical threshold for Positive definiteness in Sigma
@@ -34,7 +34,7 @@ MatK = function(x, y ,l, nu){
 #' @examples
 #' rmvToep(5, Sigma = diag(10), mu = rep(1, 10))
 #' rmvToep(10, Sigma = diag(abs(rnorm(6))), mu = rep(1, 6))
-rmvToep = function(n, Sigma, mu = NULL, eigtol = 1e-8, symtol = 1e-8){
+rmvToep = function(n, Sigma, mu = NULL, symtol = 1e-8){
    N = dim(Sigma)[1]
    if (dim(Sigma)[2] != N){
       stop("Sigma should be square matrix!")
@@ -45,23 +45,28 @@ rmvToep = function(n, Sigma, mu = NULL, eigtol = 1e-8, symtol = 1e-8){
    if (is.null(mu)){
       mu = rep(0, N)
    }
-   lambda = eigen(Sigma)$values
-   if (min(lambda) < eigtol){
+   # lambda = eigen(Sigma)$values
+   Sigma_vec = Sigma[1, ] # first row of Sigma, which contains all the values in Sigma
+   C_vec = c(Sigma_vec[1:N], Sigma_vec[N:1])
+   lambda = Re(fft(C_vec))
+   if (min(lambda) < 0){
       stop("Sigma is ill-conditioned or not Positive definite; try rmvMat or rmvRBF if applicable.")
    } else{
-      out = matrix(0, n, N)
+      m = 2*N
+      out = matrix(0, n, m)
       for (k in 1:n){
-         vec = rep(0, N)
-         vec[1] = sqrt(lambda[1]) * rnorm(1) / sqrt(N)
-         vec[(N / 2) + 1] = sqrt(lambda[(N / 2) + 1]) * rnorm(1) / sqrt(N)
+         vec = rep(0, m)
+         vec[1] = sqrt(lambda[1]) * rnorm(1) / sqrt(m)
+         vec[(m / 2) + 1] = sqrt(lambda[(m / 2) + 1]) * rnorm(1) / sqrt(m)
          i=sqrt(as.complex(-1))
-         for(j in 2:(N/2)){
+         for(j in 2:(m/2)){
             uj = rnorm(1); vj = rnorm(1)
-            vec[j] = (sqrt(lambda[j]) * (uj + i * vj)) / (sqrt(2 * N))
-            vec[N + 2 - j] = (sqrt(lambda[j]) * (uj - i * vj)) / (sqrt(2 * N))
+            vec[j] = (sqrt(lambda[j]) * (uj + i * vj)) / (sqrt(2 * m))
+            vec[m + 2 - j] = (sqrt(lambda[j]) * (uj - i * vj)) / (sqrt(2 * m))
          }
          out[k, ] = Re(fft(vec))
       }
+      out = out[, 1:N]
       out = out + matrix(mu, n, N, byrow = T)
    }
    return(out)

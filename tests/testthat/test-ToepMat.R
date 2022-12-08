21ac55aab00b0617a1fbbc68f16da67e1dee4041
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-12-08 12:42:21 +0900

    Add test for kernel functions

--- tests/testthat/test-ToepMat.R
+++ tests/testthat/test-ToepMat.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("MatK", {
+   expect_equal(MatK(1, 1, 1, 1), 1)
+   expect_equal(MatK(c(1, 2, 3), 1, rho = 2, nu = 3), c(1.0000000, 0.83910663, 0.53592547))
+})
+
+test_that("RBFK", {
+   expect_equal(RBFK(1, 1, 1), 1)
+   expect_equal(RBFK(c(1, 2, 3), 1, l = 3), c(1.0000000, 0.945959469, 0.800737403))
 })

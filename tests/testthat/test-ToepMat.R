8506991eaca16c22c5c813dad46e8b85ddf6eb01
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-12-08 12:55:05 +0900

    Add test for dimension in rmvMat output

--- tests/testthat/test-ToepMat.R
+++ tests/testthat/test-ToepMat.R
@@ -8,6 +8,13 @@ test_that("RBFK", {
    expect_equal(RBFK(c(1, 2, 3), 1, l = 3), c(1.0000000, 0.945959469, 0.800737403))
 })
 
+## check the size of the output
+
+test_that("rmvMat dimension", {
+   expect_equal(dim(rmvMat(30, c(0:50)/50, rho = 1, nu = 1)), c(30, 51))
+   expect_equal(dim(rmvMat(1, c(0:50)/50, rho = 1, nu = 1)), c(1, 51))
+})
+
 ## error expected for ill conditioned matrix
 
 test_that("error check for ill-conditioned matrix",{

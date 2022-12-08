e085bb655ff0dae66e07921f15dc95e0302e7a67
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-12-08 12:46:57 +0900

    Added condition error for rmvRBF function

--- tests/testthat/test-ToepMat.R
+++ tests/testthat/test-ToepMat.R
@@ -7,3 +7,9 @@ test_that("RBFK", {
    expect_equal(RBFK(1, 1, 1), 1)
    expect_equal(RBFK(c(1, 2, 3), 1, l = 3), c(1.0000000, 0.945959469, 0.800737403))
 })
+
+## error expected for ill conditioned matrix
+
+test_that("error check for ill-conditioned matrix",{
+   expect_error(rmvRBF(10, c(0:30)/30, l=3), "It seems that adequate circular matrix is not found. Try smaller l.")
+})
